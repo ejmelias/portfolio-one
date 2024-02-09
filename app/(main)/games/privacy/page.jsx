ce8bcf949bb162cdc94fd5ae8cd59591e736c51e
james <ejmelias@gmail.com>
2024-02-09 10:41:02 -0600

    updated game privacy page

--- app/(main)/games/privacy/page.jsx
+++ app/(main)/games/privacy/page.jsx
@@ -9,7 +9,9 @@ export default function page() {
 
     return (
         <div className="bg-gray-200/50 p-5 space-y-8 border-midnight/30 border">
-            Maze of Moros does not collect any user data.
+            Maze of Moros does not collect any user data. 
+            <br/>
+            Celesto does not collect any user data.
         </div>
     )
 }
